190cc773126055e8319e4e0f2d8de5175cfa6f18
Vinicius Araujo <90002191+viniciusaraujo05@users.noreply.github.com>
2023-04-25 10:56:52 -0300

    First commit: Configs

---
+++ App/Config/Connection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Connection;
+
+use Exception;
+use PDO;
+
+class Connection {
+
+    public $_connection = null;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        try {
+            $db_host = getenv('DB_HOST');
+            $db_name = getenv('DB_NAME');
+            $db_port = (int) getenv('DB_PORT');
+            $db_user = getenv('DB_USER');
+            $db_password = getenv('DB_PASSWORD');
+            $this->_connection = new PDO("pgsql:host={$db_host};port={$db_port};dbname={$db_name}", $db_user, $db_password, array(PDO::ATTR_PERSISTENT => true, PDO::ATTR_EMULATE_PREPARES => true));
+
+        } catch (Exception $e) {
+            
+            throw new Exception("Unable to connect to database. Error: " . json_encode(['dbhost' => $db_host, 'database_port' => $db_port, 'db_name' => $db_name]). $_SERVER['REQUEST_URI']);
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->_connection = null;
+    }
+}
