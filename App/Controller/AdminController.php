50a8e32a5bb4273f69b1bd17174e2158d43fb8b0
Vinicius Araujo <90002191+viniciusaraujo05@users.noreply.github.com>
2023-12-11 14:02:26 +0000

    Update AdminController.php

--- App/Controller/AdminController.php
+++ App/Controller/AdminController.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- *
- * @author      Webjump Core Team <dev@webjump.com.br>
- * @copyright   2022 Webjump (http://www.webjump.com.br)
- * @license     http://www.webjump.com.br Copyright
- * @link        http://www.webjump.com.br
- */
-
 declare(strict_types=1);
 
 namespace App\Controller;
